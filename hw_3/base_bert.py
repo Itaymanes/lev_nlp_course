8df78293a5d5a2ecff0f69a4c9608ecbceddf060
itay.manes <itaymanes@gmail.com>
2025-01-17 21:40:28 +0200

    change relative import

--- hw_3/base_bert.py
+++ hw_3/base_bert.py
@@ -1,7 +1,7 @@
 import re
 from torch import device, dtype
-from config import BertConfig, PretrainedConfig
-from utils import *
+from .config import BertConfig, PretrainedConfig
+from ..utils import *
 
 
 class BertPreTrainedModel(nn.Module):
